569aeac4c0ec24908364804a826f5da0f8fc2ef5
Brian Hudson <reallybrianhudson@gmail.com>
2022-01-20 17:14:51 -0500

    Bot Defeated

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import random
 
-response = requests.get("https://smile.amazon.com/Instant-Pot-Pressure-Steamer-Sterilizer/dp/B08PQ2KWHS/141-3718383-0667750")
-page_data = response.text
+url = "https://smile.amazon.com/Instant-Pot-Pressure-Steamer-Sterilizer/dp/B08PQ2KWHS/141-3718383-0667750"
+headers = {'User-Agent':
+               'Mozilla/5.0 (X11; Linux x86_64)'
+               'AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/44.0.2403.157 Safari/537.36',
+           'Accept-Language': 'en-US, en;q=0.5'
+           }
+
+response = requests.session()
+request = response.get(url, headers=headers)
+page_data = request.text
 soup = BeautifulSoup(page_data, 'html.parser')
 
+print(response)
 print(soup.prettify())
